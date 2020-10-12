c0d5ebd54821564e1fc47406250a0156362d3655
b-rachman <bobbyrachman97@outlook.com>
2020-10-12 09:55:27 +0700

    init layout and server static

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import hbs from 'hbs'
+import morgan from 'morgan'
 import path from 'path'
 
 const __dirname=path.resolve()
@@ -10,9 +11,19 @@ app.set('views',__dirname+'/layout')
 app.set('view engine','html')
 app.engine('html', hbs.__express)
 
+//log incoming request
+app.use(morgan('combined'))
+
+// serve static file
+app.use('/asset', express.static(__dirname + '/asset'))
+
 
 app.get('/',(req, res, next)=>{
     res.send({success : true})
+})
+
+app.get('/product',(req,res,next)=>{
+    res.render('product')
 })
 
 app.use((err, req, res, next)=>{
