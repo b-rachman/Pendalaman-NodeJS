216aa40bfb2f3efe8d3dbd0f19f7d2e874f1df78
b-rachman <bobbyrachman97@outlook.com>
2020-10-12 10:38:00 +0700

    Menggunakan metode POST dan install Body Parser dan pemakaiannya

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import hbs from 'hbs'
 import morgan from 'morgan'
 import path from 'path'
+import bodyParser from  'body-parser'
 
 const __dirname=path.resolve()
 
@@ -13,6 +14,9 @@ app.engine('html', hbs.__express)
 
 //log incoming request
 app.use(morgan('combined'))
+
+// parse request body
+app.use(bodyParser.urlencoded())
 
 // serve static file
 app.use('/asset', express.static(__dirname + '/asset'))
@@ -29,6 +33,12 @@ app.get('/product',(req,res,next)=>{
 //handle form GET Method
 app.get('/add-product',(req,res,next)=>{
     res.send(req.query)
+})
+
+//handle form POST Method
+app.post('/add-product',(req,res,next)=>{
+    console.log('Request',req.body)
+    res.send(req.body)
 })
 
 app.use((err, req, res, next)=>{
