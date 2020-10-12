9540886c1372f2b1d0a4d18672a2801b512c4551
b-rachman <bobbyrachman97@outlook.com>
2020-10-12 12:06:56 +0700

    Menyimpan file dan menampilkan foto pada halaman web

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import morgan from 'morgan'
 import path from 'path'
 import bodyParser from  'body-parser'
 import {initDatabase, initTable, insertProduct, getProduct} from './database.js'
+import fileUpload from 'express-fileupload'
+import fs from 'fs'
 
 const __dirname=path.resolve()
 
@@ -15,6 +17,9 @@ app.set('views',__dirname+'/layout')
 app.set('view engine','html')
 app.engine('html', hbs.__express)
 
+//use file parser
+app.use(fileUpload())
+
 //log incoming request
 app.use(morgan('combined'))
 
@@ -23,12 +28,14 @@ app.use(bodyParser.urlencoded())
 
 // serve static file
 app.use('/asset', express.static(__dirname + '/asset'))
+app.use('/files',express.static(__dirname+'/files'))
 
 
 app.get('/',(req, res, next)=>{
     res.send({success : true})
 })
 
+//get product list
 app.get('/product', async (req,res,next)=>{
     // getProduct(db).then(product =>{
     //     console.log('Product Result', product)
@@ -54,8 +61,20 @@ app.get('/add-product',(req,res,next)=>{
 //handle form POST Method
 app.post('/add-product',(req,res,next)=>{
     console.log('Request',req.body)
+    console.log('File', req.files)
+
+    //get file name
+    const fileName=Date.now()+ req.files.photo.name
+
+    fs.writeFile(path.join(__dirname, '/files', fileName), req.files.photo.data, (err)=>{
+        if (err){
+            console.error(err)
+            return
+        }
+    })
+
     //insert product
-    insertProduct(db, req.body.name, parseInt(req.body.price),'-')
+    insertProduct(db, req.body.name, parseInt(req.body.price),`/files/${fileName}`)
 
     //redirect 
     res.redirect('/product')
